4d35278cc0f6ef8790b4da4f798d18692a49f73c
nbourre <nbourrereg@hotmail.com>
2021-09-20 09:05:56 -0400

    Add project files.

---
+++ SharedModels/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharedModels
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string HomePhone { get; set; }
+
+        public override string ToString() => $"{LastName}, {FirstName}";
+    }
+}
---
+++ i18n/Views/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using i18n.ViewModels;
+using System.Windows;
+
+namespace i18n
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+    }
+}
